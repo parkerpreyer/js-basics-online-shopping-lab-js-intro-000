0ccd768632b48a638baa69c6cba1c6f8ee85403e
Parker Preyer <parker.preyer@gmail.com>
2019-11-06 01:38:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ function placeOrder(cardNumber) {
   let total = total();
   if (cardNumber) {
     cart = [];
-    return `Your total cost is ${total}, which will be charged to the card ${cardNumber}.`);
+    return `Your total cost is ${total}, which will be charged to the card ${cardNumber}.`;
   } else {
       return `Sorry, we don't have a credit card on file for you.`;
   }
