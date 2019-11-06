945750b9575c4cff54c90518d002572ae539098c
Parker Preyer <parker.preyer@gmail.com>
2019-11-06 01:28:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -63,6 +63,7 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   cart = [];
+  console.log(total());
   if (cardNumber === undefined) {
     return 'Sorry, we don\'t have a credit card on file for you.';
   } else {
