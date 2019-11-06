ab75aaff65d666ccfb5fee4eb25e5e33c9a81555
Parker Preyer <parker.preyer@gmail.com>
2019-11-06 00:54:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -51,7 +51,6 @@ function removeFromCart(item) {
   // then remove it if it matches. You might find Array.prototype.splice() to be useful.
   for (var i = 0; i < cart.length; i++) {
     let cartItem = cart[i];
-    debugger;
     if(cartItem.itemName === item) {
       cartItem.splice(cartItem, 1);
       i--;
