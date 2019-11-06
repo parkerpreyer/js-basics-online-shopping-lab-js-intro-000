dbe2a4c0897b42291b2f120114263ddf33826a94
Parker Preyer <parker.preyer@gmail.com>
2019-11-06 01:05:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -49,13 +49,16 @@ function total() {
 function removeFromCart(item) {
   // HINT: Check each object's itemName value key to see if it matches the parameter,
   // then remove it if it matches. You might find Array.prototype.splice() to be useful.
-  if(cart.includes(item)) {
-    cart.splice(i, 1);
-    return cart;
-  } else {
-      console.log("That item is not in your cart.");
+
+  for (var i = 0; i < cart.length; i++) {
+    let cartItem = cart[i];
+    if(cart.includes(item)) {
+      cart.splice(i, 1);
       return cart;
     }
+    console.log("That item is not in your cart.");
+  }
+  return cart;
 }
 
 function placeOrder(cardNumber) {
